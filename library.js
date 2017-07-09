581aa8abfb13e710ba488e1ec5609efd0038a5a5
dsottimano <dsottimano@gmail.com>
2017-07-09 23:02:36 +0200

    new

--- library.js
+++ library.js
@@ -8,31 +8,31 @@
 		{
 			"usage": "Organic keywords per URL",
             "formula" : "urlOrganicKeywords(url, includeHeader, limit, db)",
-			"example": "urlOrganicKeywords(\"http://example.com\", true, \"10\", \"us\") ",
+			"example": "urlOrganicKeywords(\"http://example.com\", true, \"10\", \"us\") "
 
 		},
         {
 			"usage": "Keyword Difficulty for a keyword",
             "formula" : "keywordDifficulty(query, includeHeader, db)",
-			"example": "keywordDifficulty(\"aparments\", false, \"us\")",
+			"example": "keywordDifficulty(\"aparments\", false, \"us\")"
 
 		},
         {
 			"usage": "Google search results for a keyword",
             "formula" : "serps(query, limit, db)",
-			"example": "serps(\"apartments\", 10, \"us\")",
+			"example": "serps(\"apartments\", 10, \"us\")"
 
 		},
         {
 			"usage": "Related keywords for a keyword",
             "formula" : "relatedQueries(query, includeHeader, limit, db)",
-			"example": "relatedQueries(\"apartments\", false, 10, \"us\")",
+			"example": "relatedQueries(\"apartments\", false, 10, \"us\")"
 
 		},
         {
 			"usage": "Search volume for a keyword",
             "formula" : "keywordVolume(query, includeHeader, db)",
-			"example": "keywordVolume(\"apartments\", false, \"us\")",
+			"example": "keywordVolume(\"apartments\", false, \"us\")"
 
 		}
 	],
