1591b708112e486712bcfcdd328714ebdd3f44f4
dsottimano <dsottimano@gmail.com>
2017-07-09 11:59:24 +0200

    test2

--- library.js
+++ library.js
@@ -1,33 +1,33 @@
-[{
-    "semrush": [
-        {
-            "name": "vvvvvdomain overview",
-            "Formula": "whatever()",
-            "Description": "blahblah",
+{
+	"semrush" : [{
+			"name": "vvvvvdomain overview",
+			"Formula": "whatever()",
+			"Description": "blahblah"
 
-        },
-        {
-            "name": "aaaaadomain overview",
-            "Formula": "whatever()",
-            "Description": "blahblah",
+		},
+		{
+			"name": "aaaaadomain overview",
+			"Formula": "whatever()",
+			"Description": "blahblah"
 
-        }
-    ],
-    "sistrix": [
-        {
-            "name": "domaisdsassn overview",
-            "Formula": "whatever()",
-            "Description": "blahblah",
+		}
+	],
 
-        },
-        {
-            "name": "domain osdvsdgvdverview",
-            "Formula": "whatever()",
-            "Description": "blahblah",
+	"sistrix": [{
+			"name": "domaisdsassn overview",
+			"Formula": "whatever()",
+			"Description": "blahblah"
 
-        }
+		},
+		{
+			"name": "domain osdvsdgvdverview",
+			"Formula": "whatever()",
+			"Description": "blahblah"
 
-    ]
+		}
+
+	]
 
 
-}]
+
+}
