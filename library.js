83f7d605cd600e1ba591b846ecb54ab78395b5e0
dsottimano <dsottimano@gmail.com>
2017-07-10 00:06:51 +0200

    new

--- library.js
+++ library.js
@@ -1,54 +1,64 @@
 {
-	"semrush": [{
+	"semrush": [
+        
+        {
+
+			"usage": "Organic hsitory for a domain",
+			"formula": "DOMAIN_OVERVIEW_SEMRUSH(domain,db,includeHeader,date)",
+			"example": "DOMAIN_OVERVIEW_SEMRUSH(\"example.com\",\"us\",false,201601)"
+
+		},
+        {
+
 			"usage": "Organic keywords for a domain",
-			"formula": "domainOrganicKeywords(domain, filterBy, matchType, query, limit, db, includeHeader, date)",
-			"example": "domainOrganicKeywords(\"example.com\", true, true, \"apartments\", 10, \"us\", true, 201601)"
+			"formula": "DOMAIN_ORGANIC_KEYWORDS_SEMRUSH(domain, filterBy, matchType, query, limit, db, includeHeader, date)",
+			"example": "DOMAIN_ORGANIC_KEYWORDS_SEMRUSH(\"example.com\", true, true, \"apartments\", 10, \"us\", true, 201601)"
 
 		},
 		{
 			"usage": "Organic keywords per URL",
-            "formula" : "urlOrganicKeywords(url, includeHeader, limit, db)",
-			"example": "urlOrganicKeywords(\"http://example.com\", true, \"10\", \"us\")"
+            "formula" : "URL_ORGANIC_KEYWORDS_SEMRUSH(url, includeHeader, limit, db)",
+			"example": "URL_ORGANIC_KEYWORDS_SEMRUSH(\"http://example.com\", true, \"10\", \"us\")"
 
 		},
         {
 			"usage": "Keyword Difficulty for a keyword",
-            "formula" : "keywordDifficulty(query, includeHeader, db)",
-			"example": "keywordDifficulty(\"aparments\", false, \"us\")"
+            "formula" : "KEYWORD_DIFFICULTY_SEMRUSH(query, includeHeader, db)",
+			"example": "KEYWORD_DIFFICULTY_SEMRUSH(\"aparments\", false, \"us\")"
 
 		},
         {
 			"usage": "Google search results for a keyword",
-            "formula" : "serps(query, limit, db)",
-			"example": "serps(\"apartments\", 10, \"us\")"
+            "formula" : "SERPS_SEMRUSH(query, limit, db)",
+			"example": "SERPS_SEMRUSH(\"apartments\", 10, \"us\")"
 
 		},
         {
 			"usage": "Related keywords for a keyword",
-            "formula" : "relatedQueries(query, includeHeader, limit, db)",
-			"example": "relatedQueries(\"apartments\", false, 10, \"us\")"
+            "formula" : "RELATED_QUERIES_SEMRUSH(query, includeHeader, limit, db)",
+			"example": "RELATED_QUERIES_SEMRUSH(\"apartments\", false, 10, \"us\")"
 
 		},
         {
 			"usage": "Search volume for a keyword",
-            "formula" : "keywordVolume(query, includeHeader, db)",
-			"example": "keywordVolume(\"apartments\", false, \"us\")"
+            "formula" : "KEYWORD_VOLUME_SEMRUSH(query, includeHeader, db)",
+			"example": "KEYWORD_VOLUME_SEMRUSH(\"apartments\", false, \"us\")"
 
 		}
 	],
 
 	"sistrix": [{
-			"name": "domaisdsassn overview",
-			"Formula": "whatever()",
-			"Description": "blahblah"
+			"usage": "Visibility Index",
+			"formula": "VISIBILITY_INDEX_SISTRIX (type,url,country,mobile,date,history,excludeHeaders)",
+			"exammple": "VISIBILITY_INDEX_SISTRIX (type,url,country,mobile,date,history,excludeHeaders)"
 
 		},
 		{
-			"name": "domain osdvsdgvdverview",
-			"Formula": "whatever()",
-			"Description": "blahblah"
+			"usage": "Keywords in Google serps for domain",
+			"formula": "DOMAIN_KEYWORD_COUNT_SISTRIX(type,url,country,mobile,date,history,excludeHeaders)",
+			"exammple": "DOMAIN_KEYWORD_COUNT_SISTRIX(\"domain\", \"example.com\", \"us\", true, 2016-09-15, true, true)"
 
-		}
+		},
 
 	]
 
