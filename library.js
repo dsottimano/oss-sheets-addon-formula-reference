cb28c1dfd3b2b31ff552376129756e5f6bb66506
dsottimano <dsottimano@gmail.com>
2017-07-09 22:58:15 +0200

    new

--- library.js
+++ library.js
@@ -1,14 +1,38 @@
 {
-	"semrush" : [{
-			"name": "vvvvvdomain overview",
-			"Formula": "whatever()",
-			"Description": "blahblah"
+	"semrush": [{
+			"usage": "Organic keywords for a domain",
+			"formula": "domainOrganicKeywords(domain, filterBy, matchType, query, limit, db, includeHeader, date)",
+			"example": "domainOrganicKeywords(\"example.com\", true, true, \"apartments\", 10, \"us\", true, 201601)"
 
 		},
 		{
-			"name": "aaaaadomain overview",
-			"Formula": "whatever()",
-			"Description": "blahblah"
+			"usage": "Organic keywords per URL",
+            "formula" : "urlOrganicKeywords(url, includeHeader, limit, db)",
+			"example": "urlOrganicKeywords(\"http://example.com\", true, \"10\", \"us\") ",
+
+		},
+        {
+			"usage": "Keyword Difficulty for a keyword",
+            "formula" : "keywordDifficulty(query, includeHeader, db)",
+			"example": "keywordDifficulty(\"aparments\", false, \"us\")",
+
+		},
+        {
+			"usage": "Google search results for a keyword",
+            "formula" : "serps(query, limit, db)",
+			"example": "serps(\"apartments\", 10, \"us\")",
+
+		},
+        {
+			"usage": "Related keywords for a keyword",
+            "formula" : "relatedQueries(query, includeHeader, limit, db)",
+			"example": "relatedQueries(\"apartments\", false, 10, \"us\")",
+
+		},
+        {
+			"usage": "Search volume for a keyword",
+            "formula" : "keywordVolume(query, includeHeader, db)",
+			"example": "keywordVolume(\"apartments\", false, \"us\")",
 
 		}
 	],
@@ -31,3 +55,6 @@
 
 
 }
+
+
+
