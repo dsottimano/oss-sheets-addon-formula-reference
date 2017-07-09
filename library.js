746eac1ea9ed79c0c5ce9f77600bc838ed04cf97
dsottimano <dsottimano@gmail.com>
2017-07-09 11:50:50 +0200

    testing

---
+++ library.js
@@ -0,0 +1,33 @@
+[{
+    semrush: [
+        {
+            name: "domain organic",
+            Formula: "domainoverview()",
+            Description: "Returns keywords for a specific domain",
+
+        },
+        {
+            name: "domain overview",
+            Formula: "whatever()",
+            Description: "blahblah",
+
+        }
+    ],
+    sistrix: [
+        {
+            name: "sistrix whatever",
+            Formula: "domainoverview()",
+            Description: "Returns keywords for a specific domain",
+
+        },
+        {
+            name: "domain sistrix",
+            Formula: "whatever()",
+            Description: "blahblah",
+
+        }
+
+    ]
+
+
+}]
